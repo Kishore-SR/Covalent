8287bcd46651a5d3799814a36867eb0d62947f1d
Kishore S R <kishoresr01@gmail.com>
2025-05-17 23:08:18 +0530

    add: signup api

--- backend/src/controllers/auth.contollers.js
+++ backend/src/controllers/auth.contollers.js
@@ -1,14 +1,73 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
 export async function signup(req, res) {
-    // Handle signup logic here
-    res.send('Signup Page');
+  const { email, password, fullName } = req.body;
+
+  try {
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "Please fill all the fields" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Please enter a valid email" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Email already exists, please use different mail" });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1;
+    const randomAvatrar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = await User.create({
+      email,
+      password,
+      fullName,
+      profilePic: randomAvatrar,
+    });
+
+    // todo : create user even in stream
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "12d",
+      }
+    );
+
+    res.cookie("jwt", token, {
+      maxAge: 12 * 24 * 60 * 60 * 1000, // 12 days
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+
+    res.status(201).json({success:true, user:newUser});
+
+  } catch (error) {
+    console.log("Error in signup controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 export async function login(req, res) {
-    // Handle login logic here
-    res.send('Login Page');
+  // Handle login logic here
+  res.send("Login Page");
 }
 
 export function logout(req, res) {
-    // Handle logout logic here
-    res.send('Logout Page');
+  // Handle logout logic here
+  res.send("Logout Page");
 }
---
+++ backend/src/models/User.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6,
+    },
+    bio:{
+        type:String,
+        default:""
+    },
+    profilePic:{
+        type:String,
+        default:""
+    },
+    nativeLanguage:{
+        type:String,
+        default:""
+    },
+    learningLanguage:{
+        type:String,
+        default:""
+    },
+    location:{
+        type:String,
+        default:""
+    },
+    isOnboarded:{
+        type:Boolean,
+        default:false
+    },
+    friends:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }]
+}, {timestamps:true});
+
+const User = mongoose.model("User", userSchema);
+
+//Pre hook - to hash the passwords of the users in database
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        return next();
+    }
+    try{
+        const salt = await bcrypt.genSalt(6);
+        this.password = await bcrypt.hash(this.password, salt);
+
+        next();
+    }
+    catch(err){
+        next(err);
+    }
+})
+
+export default User;
