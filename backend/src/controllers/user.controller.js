6d374b5a382526f039519d34832ae2bb618671bf
Kishore S R <kishoresr01@gmail.com>
2025-05-18 13:49:22 +0530

    add: friends request

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,37 @@
+import User from "../models/User.js";
+
+export async function getRecommendedUsers(req, res) {
+  try {
+    const currentUserId = req.user.id;
+    const currentUser = req.user;
+
+    const recommendedUsers = await User.find({
+      $and: [
+        { _id: { $ne: currentUserId } },
+        { $id: { $nin: currentUser.friends } },
+        { isOnboarded: true },
+      ],
+    });
+
+    res.status(200).json(recommendedUsers);
+  } catch (error) {
+    console.error("Error in getRecommendedUsers controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function getMyFriends(req, res) {
+  try {
+    const user = await User.findById(req.user.id)
+      .select("friends")
+      .populate(
+        "friends",
+        "fullName profilePicture nativeLanguage, learningLanguage"
+      );
+
+    res.status(200).json(user.friends);
+  } catch (error) {
+    console.error("Error in getMyFriends controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import authRoutes from "./routes/auth.routes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
