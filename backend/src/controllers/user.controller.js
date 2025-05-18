1bae4d6b167b89d795577079f1b43a7f1d726336
Kishore S R <kishoresr01@gmail.com>
2025-05-18 22:40:34 +0530

    add: acceptFriendRequest

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -89,3 +89,36 @@ export async function sendFriendRequest(req, res) {
     res.status(500).json({ message: "Internal server error" });
   }
 }
+
+export async function acceptFriendRequest(req, res) {
+  try{
+    const{id:requestId} = req.params;
+    const friendRequest = await FriendRequest.findById(requestId);
+
+    if(!friendRequest){
+      return res.status(404).json({message: "Friend request not found."});
+    }
+
+    if(friendRequest.recipient.toString() !== req.user.id){
+      return res.status(403).json({message: "You are not authorized to accept this friend request."});
+    }
+
+    friendRequest.status = "accepted";
+    await friendRequest.save();
+
+    // add each user to each other's friends list
+    // $addToSet is used to add a value to an array only if it doesn't already exist in the array
+    await User.findByIdAndUpdate(friendRequest.sender, {
+      $addToSet: { friends: friendRequest.recipient },
+    });
+
+    await User.findByIdAndUpdate(friendRequest.recipient, {
+      $addToSet: { friends: friendRequest.sender },
+    });
+    
+    res.status(200).json({message: "Friend request accepted."});
+  } catch (error) {
+    console.error("Error in acceptFriendRequest controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
+import { acceptFriendRequest, getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -10,5 +10,9 @@ router.get("/", protectRoute, getRecommendedUsers);
 router.get("/friends", protectRoute, getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest); //put req because we need to update the friend request
+
+router.get("/friend-request", getFriendRequests); //get all friend requests
+
 
 export default router;
