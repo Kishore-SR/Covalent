f6f85d3078bc88d40a32ae8d6437f74d9bc21222
Kishore S R <kishoresr01@gmail.com>
2025-05-18 12:24:17 +0530

    add: protectRoute middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,13 @@ import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.routes.js";
 import { connectDB } from "./lib/db.js";
-import cors from "cors";
-
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(
-  cors({
-    origin: "*", // or "*" for open
-    credentials: true,
-  })
-);
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
