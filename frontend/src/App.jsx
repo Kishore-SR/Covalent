bb686362935f37be93f21e6dcf6c936446625fe4
Kishore S R <kishoresr01@gmail.com>
2025-05-22 11:05:56 +0530

    add: lazy loader for better UX

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,24 @@
+import React, { lazy, Suspense } from "react"; // ADDED lazy and Suspense for better user-experience
 import { Navigate, Route, Routes } from "react-router";
 
-import HomePage from "./pages/HomePage.jsx";
-import SignUpPage from "./pages/SignupPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import NotificationsPage from "./pages/NotificationsPage.jsx";
-import CallPage from "./pages/CallPage.jsx";
-import ChatPage from "./pages/ChatPage.jsx";
-import OnboardingPage from "./pages/OnboardingPage.jsx";
-
 import { Toaster } from "react-hot-toast";
-import VerifyOTPPage from "./pages/VerifyOTPPage.jsx";
 import PageLoader from "./components/PageLoader.jsx";
 import useAuthUser from "./hooks/useAuthUser.jsx";
+import Layout from "./components/Layout.jsx"; 
+
+const HomePage = lazy(() => import("./pages/HomePage.jsx"));
+const SignUpPage = lazy(() => import("./pages/SignupPage.jsx"));
+const LoginPage = lazy(() => import("./pages/LoginPage.jsx"));
+const NotificationsPage = lazy(() => import("./pages/NotificationsPage.jsx"));
+const CallPage = lazy(() => import("./pages/CallPage.jsx"));
+const ChatPage = lazy(() => import("./pages/ChatPage.jsx"));
+const OnboardingPage = lazy(() => import("./pages/OnboardingPage.jsx"));
+const VerifyOTPPage = lazy(() => import("./pages/VerifyOTPPage.jsx"));
 
 const App = () => {
   const { isLoading, authUser } = useAuthUser();
 
+  // If the initial authUser data is still loading, show loading spinner
   if (isLoading) return <PageLoader />;
 
   const isAuthenticated = Boolean(authUser);
@@ -23,54 +26,57 @@ const App = () => {
 
   return (
     <div className="h-screen">
-      <Routes>
-        <Route
-          path="/"
-          element={
-            isAuthenticated && isOnboarded ? (
-                <HomePage />
-            ) : (
-              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
-            )
-          }
-        />
-        <Route
-          path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/notifications"
-          element={authUser ? <NotificationsPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/call"
-          element={authUser ? <CallPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/chat"
-          element={authUser ? <ChatPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/onboarding"
-          element={
-            isAuthenticated ? (
-              !isOnboarded ? (
-                <OnboardingPage />
+      <Suspense fallback={<PageLoader />}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isAuthenticated && isOnboarded ? (
+                <Layout showSidebar={true}>
+                  <HomePage />
+                </Layout>
               ) : (
-                <Navigate to="/" />
+                <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
               )
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-
-        <Route path="/verify-otp" element={<VerifyOTPPage />} />
-      </Routes>
+            }
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/notifications"
+            element={authUser ? <NotificationsPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/call"
+            element={authUser ? <CallPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/chat"
+            element={authUser ? <ChatPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/onboarding"
+            element={
+              isAuthenticated ? (
+                !isOnboarded ? (
+                  <OnboardingPage />
+                ) : (
+                  <Navigate to="/" />
+                )
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route path="/verify-otp" element={<VerifyOTPPage />} />
+        </Routes>
+      </Suspense>
 
       <Toaster />
     </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Create QueryClient instance
 const queryClient = new QueryClient();
